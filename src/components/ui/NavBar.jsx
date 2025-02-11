08d59fc2708f027b1a7482e6f561dc03b24c8bff
Sarthak Datta <70786858+sarthakdatta@users.noreply.github.com>
2025-02-10 18:27:42 -0800

    initial commit

---
+++ src/components/ui/NavBar.jsx
@@ -0,0 +1,96 @@
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import Lenis from "@studio-freight/lenis";
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+export default function NavBar({ sectionRefs }) {
+  const navBar = useRef(null);
+  const logo = useRef(null);
+  const cta = useRef(null);
+  const tl = gsap.timeline();
+  gsap.registerPlugin(ScrollTrigger);
+
+  useEffect(() => {
+    const lenis = new Lenis();
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  });
+
+  useEffect(() => {
+    tl.to(navBar.current, {
+      y: 0,
+      duration: 3,
+      delay: 0.5,
+      ease: "power4.inOut",
+    });
+  });
+
+
+  useEffect(() => {
+    sectionRefs.forEach((section) => {
+      ScrollTrigger.create({
+        trigger: section,
+        start: "top 375px",
+        end: "bottom 300px",
+        // markers: true,
+        animation: gsap
+          .timeline()
+          .to(navBar.current, { color: "#DDDDD5" })
+          .to(cta.current, { backgroundColor: "#D1D1C7", color: "#0E0E0C" }, 0)
+          .to(".bg-secondary-100", { backgroundColor: "#0E0E0C" }, 0),
+
+        toggleActions: "restart reverse restart reverse",
+      });
+    });
+  });
+
+  return (
+    <header
+      ref={navBar}
+      className="fixed top-0 z-50 flex w-full -translate-y-full items-center justify-between bg-secondary-100 px-5 py-3"
+    >
+      {/* logo */}
+      <a href="#hero" aria-label="Logo" className="z-50">
+        <svg
+          ref={logo}
+          width="80"
+          height="45"
+          viewBox="0 0 121 45"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          {/* Your Custom SVG Path */}
+          {/* <path d="M508.733521,559.602539 C505.535065,560.505554 502.365387,560.943115 498.931915,561.417053 C498.931915,549.996399 498.931915,539.246155 498.931915,528.332886 C502.116272,528.332886 505.076019,528.332886 508.762238,528.332886 C508.762238,538.643555 508.762238,548.890259 508.733521,559.602539 z"></path> */}
+        </svg>
+      </a>
+      <nav className=" space-x-7 font-grotesk text-body-3 sm:block">
+        <a href="#about" className="group relative hidden md:inline-block">
+          <span>about</span>
+          <span className="absolute bottom-0 left-0 h-[0.125em] w-0 rounded-full bg-secondary-600 duration-300 ease-in-out group-hover:w-full"></span>
+        </a>
+        <a href="#services" className="group relative hidden md:inline-block">
+          <span>services</span>
+          <span className="absolute bottom-0 left-0 h-[0.125em] w-0 rounded-full bg-secondary-600 duration-300 ease-in-out group-hover:w-full"></span>
+        </a>
+        <a href="#works" className="group relative hidden md:inline-block">
+          <span>projects</span>
+          <span className="absolute bottom-0 left-0 h-[0.125em] w-0 rounded-full bg-secondary-600 duration-300 ease-in-out group-hover:w-full"></span>
+        </a>
+        <a
+          ref={cta}
+          className="button group relative hover:bg-transparent"
+          href="#contact"
+        >
+          <span className="relative w-fit">
+            <span className="absolute bottom-2 h-[0.15em] w-0 bg-secondary-700 opacity-90 duration-300 ease-out group-hover:w-full"></span>
+            <span>Contact Me</span>
+          </span>
+        </a>
+      </nav>
+    </header>
+  );
+}
